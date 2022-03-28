2cdf5bc027c4d0eac0214193e958335ae2114174
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-03-29 00:01:30 +0545

    Made 3 different child components and called it inside parent CustomPage component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom";
 import myImage from "./react-logo.png";
 
-function CustomPage() {
+function HeaderElement() {
+  return (
+    <header>
+      <nav>
+        <img src={myImage} width="100px" />
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function ReasonElement() {
   return (
     <>
-      <h1>Reasons for liking React:</h1>
+      <h1>Reason for liking react.</h1>
       <ul>
         <li>Composable</li>
         <li>Declarative</li>
         <li>Managed by facebook</li>
       </ul>
+    </>
+  );
+}
+
+function FooterElement() {
+  return (
+    <footer>
+      <p>© 2022 Shrestha development. All rights reserved.</p>
+    </footer>
+  );
+}
+
+function CustomPage() {
+  return (
+    <>
+      <HeaderElement />
+      <ReasonElement />
+      <FooterElement />
     </>
   );
 }
