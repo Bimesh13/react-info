7a12655ee29dfcce03095ad4e12af1cfd062ff22
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-03-28 23:47:46 +0545

    Made first component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import myImage from "./react-logo.png";
 
-const pageContent = (
-  <div>
-    <img src={myImage} width="100px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function CustomPage() {
+  return (
+    <>
+      <h1>Reasons for liking React:</h1>
+      <ul>
+        <li>Composable</li>
+        <li>Declarative</li>
+        <li>Managed by facebook</li>
+      </ul>
+    </>
+  );
+}
 
-ReactDOM.render(pageContent, document.getElementById("root"));
+ReactDOM.render(<CustomPage />, document.getElementById("root"));
