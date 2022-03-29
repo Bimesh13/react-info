641354cb5623d1cdb690934765d184592ee8873b
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-03-29 16:37:35 +0545

    The main section challange completed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import myImage from "../images/react-logo.png";
+import reactLogo from "../images/react-icon-small.png";
 
 export default function Header() {
   return (
     <nav className="nav-bar">
-      <img src={myImage} className="react-logo" />
+      <img src={reactLogo} className="react-logo" />
       <h1>ReactFacts</h1>
       <h3>React Course - Project 1</h3>
     </nav>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <section className="main-part">
-      <div className="container">
-        <h1 className="heading">Reason for liking react.</h1>
-        <ul className="reasons">
-          <li>Composable</li>
-          <li>Declarative</li>
-          <li>Managed by facebook</li>
-        </ul>
-      </div>
-    </section>
+    <div className="main-content">
+      <h1 className="title-facts">Fun facts about React</h1>
+      <ul className="list-of-facts">
+        <li>Was released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
   );
 }
