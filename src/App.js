1c08bafa6b97f95483ab70ebc96abaad2b6f604e
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-03-29 16:07:56 +0545

    The navbar challange given in course completed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainComponent";
-import Footer from "./components/Footer";
 
 export default function CustomPage() {
   return (
     <>
       <Header />
       <MainContent />
-      <Footer />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,10 @@ import myImage from "../images/react-logo.png";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="container">
-        <nav className="nav-bar">
-          <img src={myImage} width="60px" />
-          <ul className="nav-list">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <nav className="nav-bar">
+      <img src={myImage} className="react-logo" />
+      <h1>ReactFacts</h1>
+      <h3>React Course - Project 1</h3>
+    </nav>
   );
 }
